a4e2135668df109374e870635f988d95e4acb827
Connor <connortrichards@gmail.com>
2019-09-21 03:04:16 -0700

    added prof

--- front-end/src/components/profile/Profile.js
+++ front-end/src/components/profile/Profile.js
@@ -1,11 +1,13 @@
 import React from "react";
 import BiasMeter from "./BiasMeter";
+import Header from "./Header";
 import "../../App.css";
 
 function Profile() {
   return (
     <React.Fragment>
       <section className="container">
+        <Header />
         <BiasMeter />
       </section>
     </React.Fragment>
